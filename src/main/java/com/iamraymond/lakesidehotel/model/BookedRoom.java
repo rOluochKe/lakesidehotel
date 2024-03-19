35e75b4b75286533fb9ec382228327160c6d7a58
Raymond Oluoch <rasholuoch@gmail.com>
2024-03-19 13:33:15 +0300

    data model mapping

--- src/main/java/com/iamraymond/lakesidehotel/model/BookedRoom.java
+++ src/main/java/com/iamraymond/lakesidehotel/model/BookedRoom.java
@@ -1,4 +1,66 @@
 package com.iamraymond.lakesidehotel.model;
 
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BookedRoom {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long bookingId;
+
+    @Column(name = "check_In")
+    private LocalDate checkInDate;
+
+    @Column(name = "check_Out")
+    private LocalDate checkOutDate;
+
+    @Column(name = "guest_FullName")
+    private String guestFullName;
+
+    @Column(name = "guest_Email")
+    private String guestEmail;
+
+    @Column(name = "adults")
+    private int numOfAdults;
+
+    @Column(name = "children")
+    private int numOfChildren;
+
+    @Column(name = "total_guests")
+    private int totalNumOfGuests;
+
+    @Column(name = "confirmation_Code")
+    private String bookingConfirmationCode;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "room_id")
+    private Room room;
+
+    public void calculateTotalNumberOfGuests() {
+        this.totalNumOfGuests = this.numOfAdults + numOfChildren;
+    }
+
+    public void setNumOfAdults(int numOfAdults) {
+        numOfAdults = numOfAdults;
+        calculateTotalNumberOfGuests();
+    }
+
+    public void setNumOfChildren(int numOfChildren) {
+        numOfChildren = numOfChildren;
+        calculateTotalNumberOfGuests();
+    }
+
+    public void setBookingConfirmationCode(String bookingConfirmationCode) {
+        this.bookingConfirmationCode = bookingConfirmationCode;
+    }
 }
--- src/main/java/com/iamraymond/lakesidehotel/model/Room.java
+++ src/main/java/com/iamraymond/lakesidehotel/model/Room.java
@@ -1,5 +1,50 @@
 package com.iamraymond.lakesidehotel.model;
 
-public class Room {
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import org.apache.commons.lang3.RandomStringUtils;
 
+import java.math.BigDecimal;
+import java.sql.Blob;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String roomType;
+
+    private BigDecimal roomPrice;
+
+    private Boolean isBooked = false;
+
+    @Lob
+    private Blob photo;
+
+    @OneToMany(mappedBy = "room", fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    private List<BookedRoom> bookings;
+
+    public Room() {
+        this.bookings = new ArrayList<>();
+    }
+
+    public void addBooking(BookedRoom booking) {
+        if (bookings == null) {
+            bookings = new ArrayList<>();
+        }
+
+        bookings.add(booking);
+        booking.setRoom(this);
+        isBooked = true;
+        String bookingCode  = RandomStringUtils.randomAlphanumeric(10);
+        booking.setBookingConfirmationCode(bookingCode);
+    }
 }
